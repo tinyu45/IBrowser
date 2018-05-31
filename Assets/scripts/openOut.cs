3f8122aea70fa918e57d6d99f0bfbb9e5b7006fb
timy <tinyu45O@163.com>
2018-05-31 09:08:26 +0800

    外部打开实现

---
+++ Assets/scripts/openOut.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class openOut : MonoBehaviour {
+
+	public InputField address;
+	public Button open;
+
+	// Use this for initialization
+	void Start () {
+		open.onClick.AddListener(delegate {
+			this.Open(address.text.Trim());
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	/***
+	 * 打开网页
+	 *
+	 *
+	 ***/
+	public void Open(string address)
+	{
+		Application.OpenURL (address);		
+	}
+
+
+	//跳转至浏览器打开
+	//跳转至浏览器打开
+	//跳转至浏览器打开
+	//跳转至浏览器打开
+	//跳转至浏览器打开
+	//跳转至浏览器打开
+}
